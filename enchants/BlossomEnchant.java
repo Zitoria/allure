7b83db6cc06edc9185e3f17cb7cfbf305c5ce312
hyronical <45529739+hyronical@users.noreply.github.com>
2021-08-03 19:26:50 +1000

    Update BlossomEnchant.java

--- enchants/BlossomEnchant.java
+++ enchants/BlossomEnchant.java
@@ -1,4 +1,4 @@
-package pw.hyr.allure.Enchants.Tools;
+package net.zitoria.allure.enchants.tools;
 
 import net.zitoria.allure.util.EnchantRarity;
 import org.bukkit.Tag;
@@ -8,24 +8,34 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
-import pw.hyr.allure.Models.AllureEnchant;
-import net.zitoria.allure.util.annotation.ADescription;
-import net.zitoria.allure.util.annotation.AEnchant;
-import pw.hyr.allure.Utility.EnchantUtil;
+import pw.hyr.allure.models.AllureEnchant;
+import net.zitoria.allure.util.annotation.Enchant;
+import net.zitoria.allure.util.annotation.Description;
+import pw.hyr.allure.utility.EnchantUtil;
 
-@AEnchant(name = "blossom", displayName = "Blossom", maxLevel = 4, rarity = EnchantRarity.RARE, isTreaure = true, target = EnchantmentTarget.TOOL)
-@ADescription(desc = "Drops more flowers when broken")
+/**
+* @author Hyronical
+*/
+@Enchant(id = "blossom", display = "Blossom", maxLevel = 4, rarity = EnchantRarity.RARE, treasure = true, target = EnchantmentTarget.TOOL)
+@Description(desc = "Drops more flowers when broken")
 public class BlossomEnchant extends AllureEnchant {
 
     @Override
     public void onBlockBreak(@NotNull final Player player, @NotNull final Block block, final int level, @NotNull final BlockBreakEvent event) {
-        if (EnchantUtil.passedChance(level)) {
-            if (Tag.SMALL_FLOWERS.getValues().contains(block.getType())) {
-                for (ItemStack is : block.getDrops()) {
-                    is.setAmount(is.getAmount() + level);
-                    block.getWorld().dropItemNaturally(block.getLocation(), is);
-                }
-            }
+        
+        // Give enchantment a chance of execution
+        if (!EnchantUtil.passedChance(level))
+            return;
+        
+        // Check if the block broken is that of a flower
+        if (!Tag.SMALL_FLOWERS.getValues().contains(block.getType()))
+            return;
+      
+        // Drop an added bonus of flowers based on the level
+        for (ItemStack is : block.getDrops()) {
+               is.setAmount(is.getAmount() + level);
+               block.getWorld().dropItemNaturally(block.getLocation(), is);
         }
+        
     }
 }
